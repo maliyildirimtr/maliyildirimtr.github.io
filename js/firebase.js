8d62c4924a5950a5ed45390be15c8a18d66c0a05
maliyildirimtr <maliyildirimtr@gmail.com>
2026-08-04 18:39:48 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,18 @@
 // Firebase Ayarların
 const firebaseConfig = {
-  apiKey: "AIzaSyAGh5rutIe9ryHB7ZJIuKzwZxPWLxtc6m4",
-  authDomain: "maliyildirimtr-db.firebaseapp.com",
-  projectId: "maliyildirimtr-db",
-  storageBucket: "maliyildirimtr-db.firebasestorage.app",
-  messagingSenderId: "1079425314668",
-  appId: "1:1079425314668:web:726b216d3cdeedb678cd49"
+    apiKey: "AIzaSyAGh5rutIe9ryHB7ZJIuKzwZxPWLxtc6m4",
+    authDomain: "maliyildirimtr-db.firebaseapp.com",
+    projectId: "maliyildirimtr-db",
+    storageBucket: "maliyildirimtr-db.firebasestorage.app",
+    messagingSenderId: "1079425314668",
+    appId: "1:1079425314668:web:726b216d3cdeedb678cd49"
 };
 
-// Firebase & Firestore Başlatma
-firebase.initializeApp(firebaseConfig);
+// Firebase & Firestore & Auth Başlatma
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
 const db = firebase.firestore();
+const auth = firebase.auth();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
